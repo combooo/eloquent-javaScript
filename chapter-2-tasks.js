c9e5146fb0ff5142fdd03a4d8d856e99d5eea1f6
Stanislav Kvitko <sokvitko@gmail.com>
2021-12-20 03:44:12 +0200

    solved all tasks

--- chapter-2-tasks.js
+++ chapter-2-tasks.js
@@ -1,7 +1,32 @@
-//build triangle with #
+// Looping a triangle
+for(let hash = '#'; hash.length < 8; hash += '#') {
+    console.log(hash);
+}
 
-let hash = '';
-for (let i = 0; i < 7; i++) {
-  hash += '#';
-  console.log(hash);
+// FizzBuzz
+for(let i = 1; i <= 100; i++) {
+	if(i%3 === 0 && i%5 === 0) {
+        console.log('FizzBuzz');
+        continue;
+    }
+    if(i%3 === 0) {
+        console.log('Fizz');
+        continue;
+    }
+    if(i%5 === 0) {
+        console.log('Buzz');
+        continue;
+    }
+    console.log(i);
+}
+
+//ChessBoard
+size = 8;
+for(let i = 0; i < size; i++){
+    let line = '';
+    for(let j = 0; j < size; j++) {
+        line += i%2 === 0 ? ' #' : '# ';
+    }
+    line += '\n';
+    console.log(line);
 }
